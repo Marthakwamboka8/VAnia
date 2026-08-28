37e35c81ed1cf443da840a58c22e6bbe9139bbab
Marthakwamboka8 <ericktoyianka@gmail.com>
2026-08-28 22:44:16 +0300

    Fix homepage routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import HeroCarousel from "./components/home/HeroCarousel";
 import About from "./pages/About";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HeroCarousel/>}/>
-        <Route path="/" element={<Home />} /> 
-        <Route path="/about" element={<About/>}/>
+
+        <Route path="/" element={<Home />} />
+
+        <Route path="/about" element={<About />} />
+
       </Routes>
     </BrowserRouter>
   );
